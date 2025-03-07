0551a92eb965fad1715d5e52fbccd609c65527a1
thinhschool1357 <104267661+trthinh2003@users.noreply.github.com>
2025-03-07 11:03:08 +0700

    tranningbot_day2

--- app/Http/Controllers/DialogflowController.php
+++ app/Http/Controllers/DialogflowController.php
@@ -47,33 +47,49 @@ class DialogflowController extends Controller
         $intent = $queryResult->getIntent()->getDisplayName();
         $replyMessage = $queryResult->getFulfillmentText();
 
-        //Intent iProducts
-        if ($intent === "iProducts") {
-            $parameters = json_decode($queryResult->getParameters()->serializeToJsonString(), true);
-            $productName = $parameters['product'] ?? '';
+        switch ($intent) {
+            case "iProducts":
+                $parameters = json_decode($queryResult->getParameters()->serializeToJsonString(), true);
+                $productName = $parameters['product'] ?? '';
 
-            if (!empty($productName)) {
-                $matchedProducts = DB::table('products')
-                                    ->where('product_name', 'LIKE', "%{$productName}%")
-                                    ->orWhere('description', 'LIKE', "%{$productName}%")
-                                    ->orWhere('tags', 'LIKE', "%{$productName}%")
-                                    ->select('product_name', 'price')
-                                    ->get()
-                                    ->take(5);
+                if (!empty($productName)) {
+                    $matchedProducts = DB::table('products')
+                                        ->where('product_name', 'LIKE', "%{$productName}%")
+                                        ->orWhere('description', 'LIKE', "%{$productName}%")
+                                        ->orWhere('tags', 'LIKE', "%{$productName}%")
+                                        ->select('product_name', 'price')
+                                        ->get()
+                                        ->take(5);
 
-                if ($matchedProducts->isNotEmpty()) {
-                    $productList = $matchedProducts->map(function ($product) {
-                        return "- {$product->product_name} ({$product->price} đ) </br>";
-                    })->implode("\n");
+                    if ($matchedProducts->isNotEmpty()) {
+                        $productList = $matchedProducts->map(function ($product) {
+                            return "- {$product->product_name} ({$product->price} đ) </br>";
+                        })->implode("\n");
 
-                    $replyMessage = "Các sản phẩm chúng tôi tìm được dựa trên \"" . $productName . "\":</br>" . $productList . "Bạn muốn chúng tôi tư vấn chi tiết cho từng sản phẩm không?";
+                        $replyMessage = "Một số sản phẩm mình tìm được dựa trên \"" . $productName . "\":</br>" . $productList . "Bạn muốn mình tư vấn chi tiết cho từng sản phẩm không?";
+                    } else {
+                        $replyMessage = "Xin lỗi, mình không tìm thấy sản phẩm phù hợp.";
+                    }
                 } else {
-                    $replyMessage = "Xin lỗi, chúng tôi không tìm thấy sản phẩm phù hợp.";
+                    $replyMessage = "Bạn muốn tìm sản phẩm nào? Vui lòng nói rõ hơn.";
                 }
-            } else {
-                $replyMessage = "Bạn muốn tìm sản phẩm nào? Vui lòng nói rõ hơn.";
-            }
+                break;
+            case "aboutWeb":
+                $parameters = json_decode($queryResult->getParameters()->serializeToJsonString(), true);
+                $response = $parameters['response'] ?? '';
+                if (!empty($response)) {
+                    $replyMessage = "Bên mình có bán các sản phẩm áo, từ áo vải linen, "
+                    . "vải cotton cho đến vải thun,... quần thì có đa dạng kiểu và loại từ quần short đến quần dài, quần tây. "
+                    . "Cho mình hỏi bạn đang quan tâm mẫu sản phẩm nào?";
+                } else {
+                    $replyMessage = "Đây là website TST Fashion Shop, web bên mình có đa dạng các mẫu sản phẩm quần áo và phụ kiện. "
+                    . "Các sản phẩm quần áo đều có đủ cả. Bạn muốn mình tư vấn chi tiết hơn không?";
+                }
+                break;
+            default:
+                break;
         }
+        //Intent iProducts
 
         $sessionClient->close();
 
